bc5e2b52ac2dd147e1d9047d0ad9404d59402108
ArtemLunin <lunin.mail@gmail.com>
2021-04-14 16:04:37 +0300

    Day 4. Final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6,
+      },
+      1600: {
+        slidesPerView: 5,
+      },
+      1300: {
+        slidesPerView: 4,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      800: {
+        slidesPerView: 2,
+      }
+    },
 
   
     // Navigation arrows
@@ -11,4 +29,14 @@ const swiper = new Swiper('.channel-slider', {
     },
 
   });
-  
+  const searchBtn = document.querySelector('.mobile-search');
+  const mobileSearch = document.querySelector('.input-group');
+  searchBtn.addEventListener('click', () => {
+    mobileSearch.classList.toggle('is-open');
+  });
+
+  if (document.documentElement.scrollWidth <= 640) {
+    swiper[0].destroy();
+    swiper[1].destroy();
+    swiper[2].destroy();
+  }
